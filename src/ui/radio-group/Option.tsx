564358bd7b50fcbb1365a9e6a26fd2527cdfc397
Kagura FrontEnd <kuzinaflkuzina@gmail.com>
2025-08-27 00:11:14 +0700

    final

--- src/ui/radio-group/Option.tsx
+++ src/ui/radio-group/Option.tsx
@@ -18,7 +18,6 @@ export const Option = (props: OptionProps) => {
 	const {value, title, selected, groupName, onChange, option} = props;
 
 	const optionRef = useRef<HTMLDivElement>(null);
-
 	const handleChange = () => onChange?.(option);
 
 	useEnterSubmit({onChange, option});
